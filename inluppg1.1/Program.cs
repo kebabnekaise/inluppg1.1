ddd61452faf3e71ea39eb575180ecdb77d3ced2c
emsi06001 <emsi06001@E-PW0234N3>
2023-09-06 12:31:04 +0200

    Add project files.

---
+++ inluppg1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace inluppg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Denna delen skriver ut den första textbiten. Den använder WriteLine-commandot.
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+
+            //Denna delen skriver ut den andra textbiten.
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");
+
+            //Denna delen gör så att konsollen inte ska skriva ut text efter texten jag skrev.
+            Console.ReadKey();
+        }
+    }
+}
